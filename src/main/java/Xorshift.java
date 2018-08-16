69fcd5fad706f3714f3a28fbf69c8fcacea252c6
lytles@takashi <srl.source@nqzero.com>
2018-08-15 21:17:57 -0400

    cleanup - merge X2 into Xorshift

--- src/main/java/Xorshift.java
+++ src/main/java/Xorshift.java
@@ -6,15 +6,56 @@
 
 
 
+/*
+    from wikipedia:
+    uint64_t x = s[0];
+    uint64_t const y = s[1];
+    s[0] = y;
+    x ^= x << 23; // a
+    s[1] = x ^ y ^ (x >> 17) ^ (y >> 26); // b, c
+    return s[1] + y;            
+*/
+
+
+
+
 /**
- *  a demo and benchmark of kilim generators used to implement the xorshift PRNG
+ *  a demo and benchmark of Project Loom continuations used to implement the xorshift PRNG
  *  runs xorshift a number of cycles, printing the nanos per cycle and the xor of the result
  *  the value is printed just to ensure that the JIT actually runs all the code
  *  https://en.wikipedia.org/wiki/Xorshift#xorshift.2B
- *  with modes to run the same algorithm as a Generator and as a raw Continuation (the default)
  */
 public class Xorshift {
     static final ContinuationScope SCOPE = new ContinuationScope() {};
+    Continuation ctu = new Continuation(SCOPE,this::execute);
+    long result;
+
+    void cycle(long num) {
+        final long start = System.nanoTime();
+        long val = loop(num);
+        long duration = System.nanoTime() - start;
+        System.out.format("%-10.2f nanos/op, %30d\n", 1.0*duration/num, val);
+    }
+    public long loop(long num) {
+        long val = 0;
+        for (int ii=0; ii < num && !ctu.isDone(); ii++) {
+            ctu.run();
+            val = val ^ result;
+        }
+        return val;
+    }
+    public void execute() {
+        long x, y, s0=103, s1=17;
+        while (true) {
+            x = s0;
+            y = s1;
+            s0 = y;
+            x ^= (x << 23);
+            s1 = x ^ y ^ (x >> 17) ^ (y >> 26);
+            result = (s1 + y);
+            Continuation.yield(SCOPE);
+        }
+    }
 
     public static void main(String[] args) {
         long cycles = 5000000;
@@ -26,49 +67,9 @@ public class Xorshift {
         try { cycles = Long.parseLong(args[0]); } catch (Exception ex) {}
         try { reps = Integer.parseInt(args[1]); } catch (Exception ex) {}
 
-        X2 primes = new X2();
-
+        Xorshift xor = new Xorshift();
+        
         for (int jj=0; jj < reps; jj++)
-            cycle(primes,cycles);
+            xor.cycle(cycles);
     }
-    static void cycle(X2 looper,long num) {
-        final long start = System.nanoTime();
-        long val = looper.loop(num);
-        long duration = System.nanoTime() - start;
-        System.out.format("%-10.2f nanos/op, %30d %s\n", 1.0*duration/num, val, looper.getClass().getSimpleName());
-    }
-
-    public static class X2 {
-        Continuation ctu = new Continuation(SCOPE,this::execute);
-        long result;
-        public void execute() {
-            long x, y, s0=103, s1=17;
-            while (true) {
-                x = s0;
-                y = s1;
-                s0 = y;
-                x ^= (x << 23);
-                s1 = x ^ y ^ (x >> 17) ^ (y >> 26);
-                result = (s1 + y);
-                Continuation.yield(SCOPE);
-            }
-        }
-        public long loop(long num) {
-            long val = 0;
-            for (int ii=0; ii < num && !ctu.isDone(); ii++) {
-                ctu.run();
-                val = val ^ result;
-            }
-            return val;
-        }
-    }
-    /*
-        from wikipedia:
-        uint64_t x = s[0];
-        uint64_t const y = s[1];
-        s[0] = y;
-        x ^= x << 23; // a
-        s[1] = x ^ y ^ (x >> 17) ^ (y >> 26); // b, c
-        return s[1] + y;            
-    */
 }
