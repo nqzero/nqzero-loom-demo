b5fc9be3bac82460605b89f8d81c7d5ef494bda5
lytles@takashi <srl.source@nqzero.com>
2018-09-18 12:23:50 -0400

    loom - add XorDeep from Hamlin Li (loom mailing list)

---
+++ XorDeep.java
@@ -0,0 +1,108 @@
+public class Xorshift {
+    static final ContinuationScope SCOPE = new ContinuationScope() {};
+    Continuation ctus[];
+    long result;
+    int depth;
+    boolean debug;
+    int ctuNum;
+    double sum;
+    long times;
+
+    void average() {
+        System.out.format("==== loom average : %-10.2f nanos/op%n", sum/times); 
+    }
+ 
+    Xorshift(int depth, int ctuNum, boolean debug) {
+        this.depth = depth;
+        this.ctuNum = ctuNum;
+        this.debug = debug;
+        ctus = new Continuation[ctuNum];
+        for (int i = 0; i < ctuNum; i++) {
+            ctus[i] = new Continuation(SCOPE, this::recursiveCall);
+        }
+    }
+
+    void warmup(long num) {
+        long warmup = 5000000000L;
+        final long start = System.nanoTime();
+        long dummy = 0;
+        while (System.nanoTime() - start < warmup)
+            dummy = dummy ^ loop(num);
+        System.out.println("warmup: " + dummy);
+    }
+    void cycle(long num) {
+        final long start = System.nanoTime();
+        long val = loop(num);
+        long duration = System.nanoTime() - start;
+        double tmp = 1.0*duration/num;
+        sum += tmp;
+        times++; 
+        System.out.format("loom : %-10.2f nanos/op, %30d\n", tmp, val);
+    }
+    public long loop(long num) {
+        long val = 0;
+        for (int ii=0; ii < num; ii++) {
+            ctus[ii%ctuNum].run();
+            val = val ^ result;
+        }
+        return val;
+    }
+
+    void recursiveCall() {
+        //System.out.println("initial d: " + depth);
+        recursiveCall(depth); 
+    }
+    void recursiveCall(int d) {
+        //System.out.println("d: " + d);
+        while(d > 0) {
+            recursiveCall(--d);
+        }
+        execute();
+    }
+    public void execute() {
+        if (debug) {
+            new Throwable().printStackTrace();
+        }
+        long x, y, s0=103, s1=17;
+        while (true) {
+            x = s0;
+            y = s1;
+            s0 = y;
+            x ^= (x << 23);
+            s1 = x ^ y ^ (x >> 17) ^ (y >> 26);
+            result = (s1 + y);
+            Continuation.yield(SCOPE);
+        }
+    }
+
+    public static void main(String[] args) {
+
+        long cycles = 200000;
+        int reps = 10;
+        int depth = 0;
+        boolean debug = false;
+        int ctuNum = 1;
+        
+        try { cycles = Long.parseLong(args[0]); } catch (Exception ex) {}
+        try { reps = Integer.parseInt(args[1]); } catch (Exception ex) {}
+        try { depth = Integer.parseInt(args[2]); } catch (Exception ex) {}
+        try { ctuNum = Integer.parseInt(args[3]); } catch (Exception ex) {}
+        try { debug = Boolean.parseBoolean(args[4]); } catch (Exception ex) {}
+
+        if (args.length == 0) {
+            System.out.println("args: number of cycles, number of repeats, depth of recursive calls, continuation number, debug");
+            System.out.format("\t no args provided using defaults: %d %d %d %d %b\n",cycles, reps, depth, ctuNum, debug);
+        } else {
+            System.out.format("\t cycle: %d, reps: %d, depth: %d, ctuNum: %d, debug: %b\n",cycles, reps, depth, ctuNum, debug);
+        }
+
+        new Xorshift(depth, ctuNum, debug).warmup(cycles);
+        Xorshift xor = new Xorshift(depth, ctuNum, debug);
+        
+        for (int jj=0; jj < reps; jj++) {
+            //Xorshift xor = new Xorshift(depth, ctuNum, debug);
+            xor.cycle(cycles);
+        }
+        xor.average();
+    }
+}
